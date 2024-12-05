7541130059a108f07f8b164737a4b77e9acad45b
Naznin <nazninakhtar17@gmail.com>
2024-12-05 20:25:30 +0530

    home page created

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use(errorHandler);
-
+app.get("/", (req, res) => {
+    res.send("Welcome to Contact Manager API");
+})
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
 })
